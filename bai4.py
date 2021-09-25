58405f67cfe75d57f4d8edc2b73b3cd4e68f8cce
chiến nguyễn <90491127+pettpet@users.noreply.github.com>
2021-09-25 10:51:55 +0700

    Add files via upload

--- bai4.py
+++ bai4.py
@@ -1,4 +1,4 @@
-def tongn(n):
+def tong(n):
     total = 0;
     while (n > 0):
         total = total + n % 10;
@@ -6,4 +6,4 @@ def tongn(n):
     return total;
  
 n = int(input("Nhập số nguyên dương n = "));
-print("Tổng các chữ số của", n , "là",tongn(n));
+print("Tổng các chữ số của", n , "là",tong(n));
