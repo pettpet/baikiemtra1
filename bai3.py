e0f3e3c4e361920470eb449f5b36a19786f5c403
chiến nguyễn <chienthanmkqn@gmail.com>
2021-09-25 10:01:23 +0700

    bài kiểm tra

---
+++ bai3.py
@@ -0,0 +1,13 @@
+def tinhgiaithua(n):
+    giai_thua = 1;
+    if (n == 0 or n == 1):
+        return giai_thua;
+    else:
+        for i in range(2, n + 1):
+            giai_thua = giai_thua * i;
+        return giai_thua;
+n = int(input("Giai thừa n = "));
+if n <= 0:
+ print("Nhập lại")
+else:
+   print("Giai thừa của", n, "là", tinhgiaithua(n))
